2f707ae8a25ed78eb1160fcfe1ed00c8d21270c3
Zilton7 <zilasino27@gmail.com>
2018-07-27 23:02:48 +0200

    starting the project

---
+++ arbitrage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# This program buys some Dogecoins and sells them for a bigger price
+from bittrex import bittrex
+
+# Key
+# debe0cf8e5ec4ab9a45dbbfb490e5c4d
+# Secret
+# 92a8dc78bb664762aba12886e05dac51
+
+
+# Get these from https://bittrex.com/Account/ManageApiKey
+api = bittrex('debe0cf8e5ec4ab9a45dbbfb490e5c4d', '92a8dc78bb664762aba12886e05dac51')
+
+# Market to trade at
+trade = 'BTC'
+currency = 'DOGE'
+market = '{0}-{1}'.format(trade, currency)
+# Amount of coins to buy
+amount = 100
+# How big of a profit you want to make
+multiplier = 1.1
+
+# Getting the BTC price for DOGE
+dogesummary = api.getmarketsummary(market)
+dogeprice = dogesummary[0]['Last']
+print('The price for {0} is {1:.8f} {2}.'.format(currency, dogeprice, trade))
+
+# Buying 100 DOGE for BTC
+print('Buying {0} {1} for {2:.8f} {3}.'.format(amount, currency, dogeprice, trade))
+api.buylimit(market, amount, dogeprice)
+
+# Multiplying the price by the multiplier
+dogeprice = round(dogeprice*multiplier, 8)
+
+# Selling 100 DOGE for the  new price
+print('Selling {0} {1} for {2:.8f} {3}.'.format(amount, currency, dogeprice, trade))
+api.selllimit(market, amount, dogeprice)
+
+# Gets the DOGE balance
+dogebalance = api.getbalance(currency)
+print("Your balance is {0} {1}.".format(dogebalance['Available'], currency))
+
+# For a full list of functions, check out bittrex.py or https://bittrex.com/Home/Api
---
+++ my_bittrex_api.py
@@ -0,0 +1,35 @@
+import urllib.request as urllib
+import json
+import os, ssl
+
+
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+    getattr(ssl, '_create_unverified_context', None)):
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+
+
+def get_market_data():
+    print('Choose market. example: "BTC-OMG or USD-BTC"')
+    market = input('> ')
+    url = f"https://bittrex.com/api/v1.1/public/getticker?market={market}"
+    json_obj = urllib.urlopen(url)
+    data = json.load(json_obj)
+    if data['success'] == False:
+        print('Invalid input')
+        get_market_data()
+    else:
+        #print(data)
+        ask_data = data['result']['Bid']
+        bid_data = data['result']['Ask']
+        last_data = data['result']['Last']
+        print(market)
+        print('ASK:' 8f(ask_data)
+        print(f'BID: {bid_data}')
+        print(f'LAST: {last_data}')
+        get_market_data()
+
+
+get_market_data()
+
+
